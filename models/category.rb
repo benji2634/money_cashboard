842d36dfe2a9b9032dae71554a32376c64ecab53
Tom Benham <benji26@hotmail.co.uk>
2016-10-22 15:28:02 +0100

    created class methods for category

--- models/category.rb
+++ models/category.rb
@@ -17,4 +17,30 @@ class Category
     @id = category['id'].to_i
   end
 
+  def self.all
+    sql = "SELECT * FROM categories"
+    return Category.map_items(sql)
+  end
+
+  def self.find
+    sql = "SELECT * FROM categories WHERE id = #{@id}"
+    return Category.map_item(sql)
+  end
+
+  def self.delete_all
+    sql = "DELETE FROM categories"
+    SqlRunner.run(sql)
+  end
+
+  def self.map_items(sql)
+    categories = SqlRunner.run(sql)
+    result = categories.map { |category| Category.new(category) }
+    return result 
+  end
+
+  def self.map_item(sql)
+    result = Category.map_items(sql)
+    return result.first
+  end
+
 end
